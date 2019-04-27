6299618d0ba7e0a4d93ff3756e3c0c93c2b9c429
bakullama <haggarf@protonmail.com>
2019-04-27 22:39:46 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import RaceData
+import time
+import requests
+import lxml.html as lh
+from datetime import datetime
+from prettytable import PrettyTable
+from os import system
+
+def main():
+
+if __name__ == "__main__":
+    main()
