38973907ad3467633fb5565dfe4b87b90ed10a4b
bakullama <haggarf@protonmail.com>
2019-04-29 19:36:36 +0100

    commit

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import DataGrab
 import argparse
+import time
+import requests
+import lxml.html as lh
+from datetime import datetime
+from prettytable import PrettyTable
+from os import system
+
 
 countries = ["australia", "bahrain", "china", "azerbaijan", "spain", "monaco",
              "canada", "france", "austria", "great_britain", "germany", "hungary",
              "belgium", "italy", "singapore", "russia", "japan", "mexico",
              "united_states", "brazil", "abu_dhabi"]
+
 
 def main(dataType, country, raceID):
     if dataType == 0:
@@ -22,11 +29,11 @@ def main(dataType, country, raceID):
         print("Invalid flags")
 
     try:
-        data = DataGrab.getdata(url)
+        data = getdata(url)
     except IndexError:
         print("An error occured, are you sure this race has happened yet?")
         exit()
-    DataGrab.display(data, headers, waitTime)
+    display(data, headers, waitTime)
 
 
 def getRaceID(country):
@@ -34,7 +41,6 @@ def getRaceID(country):
     found = False
     if country == None:
         return None
-
     else:
         for i in range(len(countries)):
             if countries[i] == country:
@@ -45,6 +51,50 @@ def getRaceID(country):
     else:
         print("invalid country (for countries with multiple words, use an _, ie great_britain)")
         exit()
+
+
+def getdata(url):
+
+        page = requests.get(url)
+        doc = lh.fromstring(page.content)
+        tbody = doc.xpath("//tbody")
+        dataraw = []
+        print(tbody)
+
+        for t in tbody[0]:
+            name = t.text_content()
+            dataraw.append(name)
+        data = []
+
+        for j in range(20):
+            data.append([])
+            popped = 0
+            row = dataraw[j].split("\n")
+            for i in range(len(row)):
+                row[i - popped] = row[i - popped].strip()
+                if row[i - popped] == "":
+                    row.pop(i - popped)
+                    popped += 1
+            if "race-result" in url:
+                row.pop(4)
+            else:
+                row.pop(3)
+            data[j] = row
+
+        return data
+
+
+def display(data, headings, waitTime):
+    while True:
+        system("clear")
+        t = PrettyTable(headings)
+        for i in data:
+            t.add_row(i)
+        print(t)
+        print("local time:")
+        print(datetime.now().strftime('%H:%M'))
+        time.sleep(waitTime)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
